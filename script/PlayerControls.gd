7ec534c1ea3cdeb3504c016aa99a7e451e290e49
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-26 09:09:03 +0800

    added multiplayer controls

---
+++ script/PlayerControls.gd
@@ -0,0 +1,7 @@
+class_name PlayerControls
+extends Resource
+
+@export var player_index: int = 0
+@export var move_left: String = "ui_left"
+@export var move_right: String = "ui_right"
+@export var jump: String = "ui_accept"
--- script/player.gd
+++ script/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -350.0
+var direction
+@export var controls: Resource = null
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,12 +16,13 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed(controls.jump) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	direction = Input.get_axis(controls.move_left, controls.move_right)		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
